eb416460cb27c863fd8386beb5865508b3e87111
lazyymohitt <async.mohitt@gmail.com>
2026-06-27 13:04:33 +0530

    feat: Implement authentication context and custom hook for user management

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import router from "./app.routes"
 import {RouterProvider} from 'react-router-dom'
 
+import { AuthProvider } from "./Features/auth/auth.context"
+
 import "./Features/shared/styles/global.scss"
 
 function App() {
 
   return (
-    <>
+    <AuthProvider>
     <RouterProvider router={router}/>
-    </>
+    </AuthProvider>
   )
 }
 
---
+++ Frontend/src/Features/auth/hooks/useAuth.js
@@ -0,0 +1,29 @@
+import { login, register, logout, getMe } from "../services/auth.api";
+
+import { use, useContext } from "react";
+
+import { AuthContext } from "../auth.context";
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+
+  const { user, setUser, loading, setLoading } = context;
+
+  async function handleRegister(username, email, password) {
+    setLoading(true);
+
+    const data = await register(username, email, password);
+    setUser(data.user);
+
+    setLoading(false);
+  }
+
+  async function handleLogin(username, email, password) {
+    setLoading(true);
+    
+    const data = await login(username, email, password);
+    setUser(data.user);
+
+    setLoading(false);
+  }
+};
