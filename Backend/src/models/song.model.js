faab5db270236e5a3e874e51e9a025ed59bda667
lazyymohitt <async.mohitt@gmail.com>
2026-07-03 00:49:21 +0530

    feat: Add mood field to song model with enum validation

--- Backend/src/models/song.model.js
+++ Backend/src/models/song.model.js
@@ -13,6 +13,13 @@ const songSchema = new mongoose.Schema({
     title:{
         type:String,
         required:true
+    },
+    mood:{
+        type:String,
+        enum:{
+            values:["sad","happy", "surprise"],
+            message:"this is Enum"
+        }
     }
 }) 
 
