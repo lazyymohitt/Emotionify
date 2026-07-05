6aea5c16a521c73bf9897dc6b7db75b6823b5c02
lazyymohitt <async.mohitt@gmail.com>
2026-07-06 01:26:44 +0530

    feat: Reintroduce AuthContext and router configuration for authentication flow

---
+++ Frontend/src/app/router.jsx
@@ -0,0 +1,27 @@
+import {createBrowserRouter} from "react-router-dom"
+import Login from "../features/auth/pages/Login"
+import Register from "../features/auth/pages/Register"
+import Protected from "../features/auth/components/Protected"
+ const router  = createBrowserRouter([
+     {
+        path:"/",
+        element: <Protected><h1>Home</h1></Protected>
+     }
+     ,
+     {path:"/login",
+        element:<Login/>
+     }
+     ,
+     {
+        path:"/register",
+        element:<Register/>
+     }
+
+
+]
+
+
+
+)
+
+export default router
