0e44e32d464d35c13d63c1ce2867b99bc63fb093
lazyymohitt <async.mohitt@gmail.com>
2026-07-09 12:11:39 +0530

    feat: Implement Navbar component with search input, weather display, and user info

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import "./navbar.scss"
 
 const Navbar = () => {
   return (
-   <nav>
-    Navbar
-   </nav>
+   
+       <header className="navbar">
+
+        <div className="navbar__left">
+
+        <input className='navbar__search' type="text" placeholder='Search songs , artists...' />
+
+        </div>
+
+        <div className="navbar__right">
+
+        <div className="navbar__weather">
+
+          🌤 28°C
+
+        </div>
+
+        <button className="navbar__theme">
+
+          🌙
+
+        </button>
+
+        <div className="navbar__user">
+
+          Mohit
+
+        </div>
+
+      </div>
+       </header>
   )
 }
 
